aea217243d3df097f1da76a40363f9eb666ccb04
Adam Dawkins <adam@dragondrop.uk>
2019-08-08 18:05:38 +0100

    Create affected body parts with user

--- app/controllers/body_parts_controller.rb
+++ app/controllers/body_parts_controller.rb
@@ -1,11 +1,12 @@
 class BodyPartsController < ApplicationController
   def index
     @body_parts = BodyPart.all
+    session[:body_part_ids] ||= []
   end
 
   def update
     body_part_ids = session[:body_part_ids] || []
-    body_part_ids << params[:id]
+    body_part_ids << params[:id].to_i
     session[:body_part_ids] = body_part_ids.uniq
     
     respond_to do |format|
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -5,9 +5,12 @@ class UsersController < ApplicationController
 
   def create
     @user = User.new(user_params)
-
+     
     respond_to do |format|
       if @user.save
+        session[:body_part_ids].each do |id|
+          @user.affected_body_parts.create(body_part_id: id)
+        end
         format.html { redirect_to user_affected_body_parts_path(@user) }
       else
         format.html { render :new }
