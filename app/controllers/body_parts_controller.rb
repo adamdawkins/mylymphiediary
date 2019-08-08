5761b148ef32d39653bfcb37165728dd83327443
Adam Dawkins <adam@dragondrop.uk>
2019-08-08 17:15:37 +0100

    Create body parts page and store ids in session

---
+++ app/controllers/body_parts_controller.rb
@@ -0,0 +1,15 @@
+class BodyPartsController < ApplicationController
+  def index
+    @body_parts = BodyPart.all
+  end
+
+  def update
+    body_part_ids = session[:body_part_ids] || []
+    body_part_ids << params[:id]
+    session[:body_part_ids] = body_part_ids.uniq
+    
+    respond_to do |format|
+      format.html { redirect_to body_parts_path }
+    end
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,8 @@
 Rails.application.routes.draw do
+  resources :body_parts, only: [:index, :update, :destroy]
   resources :users do
   resources :affected_body_parts
   end
-  root to: "users#index"
+  root to: "body_parts#index"
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
