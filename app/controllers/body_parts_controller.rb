4cb4bcba7c6428309f88ef64e21cff128d700889
Adam Dawkins <adam@dragondrop.uk>
2019-10-09 21:28:38 +0100

    ✅ Allow body parts to be de-selected

--- app/controllers/body_parts_controller.rb
+++ app/controllers/body_parts_controller.rb
@@ -13,4 +13,13 @@ class BodyPartsController < ApplicationController
       format.html { redirect_to body_parts_path }
     end
   end
+
+  def destroy
+    body_part_ids = session[:body_part_ids] - [params[:id].to_i]
+    session[:body_part_ids] = body_part_ids.uniq
+    
+    respond_to do |format|
+      format.html { redirect_to body_parts_path }
+    end
+  end
 end
